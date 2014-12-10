8c3eb480934f567a98ddcd051328fed3b7b24318
PoireauRenegat <jonathan.roger@free.fr>
2014-12-10 22:17:59 +0100

    Modification de la directive heightify pour modifier la hauteur en live

--- directives/app.directives.resizable.js
+++ directives/app.directives.resizable.js
@@ -6,10 +6,15 @@
 
   	function heightify() {
 	    return {
+	    	scope : {
+	    		duration : "=heightify"
+	    	},
 	    	link : function (scope, element) {
 	        	//Un jour = 28800s = 800px 
-        		var height = scope.task.duration * 800 / 28800;
-	        	element.height(height);
+	        	scope.$watch('duration', function (){
+	        		var height = scope.duration * 800 / 28800;
+		        	element.height(height);
+	        	});
 		    }
 		}
 	}
