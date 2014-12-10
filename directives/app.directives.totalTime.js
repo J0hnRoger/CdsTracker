7cab1b016f91cc6e284239f8b5f2a84e355c716a
PoireauRenegat <jonathan.roger@free.fr>
2014-12-11 00:56:39 +0100

    AJout de la directive pour calculer le rapport entre le temps passé sur une tache par rapport au temps passé sur la journée

--- directives/app.directives.totalTime.js
+++ directives/app.directives.totalTime.js
@@ -3,9 +3,11 @@
 
     angular
         .module('app.directives.totalDay', [])
-        .directive('totalTime', totalTime);
+        .directive('totalTime', totalTime)
+        .directive('decimalPassedTime', decimalPassedTime);
 
-    function totalTime ($filter) {
+    //$timeout for prevent  "$digest already in progress" error
+    function totalTime ($filter, $timeout) {
         var directive = {
             scope : {
                 tasks : "="
@@ -20,8 +22,40 @@
                 var total = 0;
                 angular.forEach(scope.tasks, function(task){
                     total += parseInt(task.duration);
-                })
-                element.empty().append($filter('friendlyHour')(total));
+                });
+
+                $timeout(function() {
+                    element.empty().append($filter('friendlyHour')(total));
+                }, 0);
+            }, true);
+        }
+
+        return directive;
+    }
+
+
+    //$timeout for prevent  "$digest already in progress" error
+    function decimalPassedTime ($filter, $timeout) {
+        var directive = {
+            scope : {
+                tasks : "=",
+                time : "="
+            },
+            link: linkFunction,
+            restrict : 'E'
+        };
+
+        function linkFunction(scope, element, attrs){
+            
+            scope.$watch('tasks', function(){
+                var total = 0;
+                angular.forEach(scope.tasks, function(task){
+                    total += parseInt(task.duration);
+                });
+
+                $timeout(function() {
+                    element.empty().append($filter('passedTime')(scope.time, total));
+                }, 0);
             }, true);
         }
 
